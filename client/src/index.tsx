9bbdfcf66531bb485eb678ea2ad621089c43d1f4
Kahraman <80291704+kahramanX@users.noreply.github.com>
2022-12-07 16:32:15 +0300

    absolute path edited

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./index.scss";
-import App from "./App";
+import App from "App";
+import "assets/styles/index.scss";
 
 const root: ReactDOM.Root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
