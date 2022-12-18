d5c102536316a9acf29808faeba1fff880ec7158
Kahraman <80291704+kahramanX@users.noreply.github.com>
2022-12-18 08:38:50 +0300

    some edit

---
+++ client/src/pages/HomePage/index.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { Helmet } from "react-helmet";
+
+type Props = {};
+
+const HomePage: React.FC = (props: Props) => {
+  return (
+    <>
+      <Helmet>
+        <title>MR.SCRUBBER</title>
+      </Helmet>
+      <div>HomePage</div>;
+    </>
+  );
+};
+
+export default HomePage;
