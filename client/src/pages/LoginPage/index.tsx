8b3b78f70ccbd8eb63058254672f79c5c7ae7747
Kahraman <80291704+kahramanX@users.noreply.github.com>
2022-12-29 23:05:47 +0300

    layered input & button styles bug fixed

--- client/src/pages/LoginPage/index.tsx
+++ client/src/pages/LoginPage/index.tsx
@@ -1,6 +1,9 @@
 import React from "react";
 import { Helmet } from "react-helmet-async";
 import "assets/styles/components/loginPage/loginPage.scss";
+import FlatInput from "components/shared/FlatInput";
+import LayeredInput from "components/shared/LayeredInput";
+import LayeredButton from "components/shared/LayeredButton";
 
 const LoginPage: React.FC = () => {
   return (
@@ -12,7 +15,13 @@ const LoginPage: React.FC = () => {
         <div className="page-title">
           <h1>Login 🔑</h1>
         </div>
-        <div className="page-content"></div>
+        <div className="page-content">
+          <LayeredInput inputType={"text"} placeholderText={"E-mail"} />
+          <br />
+          <LayeredInput inputType={"text"} placeholderText={"Password"} />
+
+          <LayeredButton text="Send" textSize="md" />
+        </div>
       </div>
     </>
   );
