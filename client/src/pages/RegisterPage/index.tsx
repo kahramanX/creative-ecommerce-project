d3f6be7365977ee55886c4d3a2d602a52f63711b
Kahraman <80291704+kahramanX@users.noreply.github.com>
2022-12-29 23:45:21 +0300

    register and login page added

--- client/src/pages/RegisterPage/index.tsx
+++ client/src/pages/RegisterPage/index.tsx
@@ -1,5 +1,8 @@
+import LayeredButton from "components/shared/LayeredButton";
+import LayeredInput from "components/shared/LayeredInput";
 import React from "react";
 import { Helmet } from "react-helmet-async";
+import { Link } from "react-router-dom";
 
 const RegisterPage: React.FC = () => {
   return (
@@ -11,7 +14,23 @@ const RegisterPage: React.FC = () => {
         <div className="page-title">
           <h1>Register 🔓🔑</h1>
         </div>
-        <div className="page-content"></div>
+        <div className="page-content">
+          <form>
+            <LayeredInput inputType={"text"} placeholderText={"Firstname"} />
+            <br />
+            <LayeredInput inputType={"text"} placeholderText={"Lastname"} />
+            <br />
+            <LayeredInput inputType={"text"} placeholderText={"E-mail"} />
+            <br />
+            <LayeredInput inputType={"text"} placeholderText={"Password"} />
+            <br />
+            <LayeredButton text="Register" textSize="md" />
+          </form>
+          <br />
+          <div className="short-link">
+            Aldredy have an account? <Link to={"/login"}>Login</Link>
+          </div>
+        </div>
       </div>
     </>
   );
