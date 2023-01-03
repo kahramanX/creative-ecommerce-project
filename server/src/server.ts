aa81dd09b65114bd173709e872bb087aa1dc9efa
Kahraman <80291704+kahramanX@users.noreply.github.com>
2023-01-03 20:04:17 +0300

    db connected and tested

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,34 +1,60 @@
 import express, { Request, Response } from "express";
 import * as dotenv from "dotenv";
 import cors from "cors";
+import { Sequelize } from "sequelize";
 
 const app = express();
 
 // THIS CODES FIX THAT CORS ERROR
-app.use(
-  cors({
-    origin: process.env.ORIGIN,
-    methods: ["GET", "POST", "DELETE", "UPDATE", "PUT", "PATCH"],
-  })
-);
+app.use(cors());
+
+const corsOptions = {
+  origin: "*",
+  optionsSuccessStatus: 200,
+  methods: ["GET", "DELETE", "POST", "PUT", "PATCH"],
+  allowedHeaders: ["Content-Type"],
+};
 
 dotenv.config();
 
-app.use(express.json({ limit: "50mb" }));
-app.use(express.urlencoded({ extended: true, limit: "50mb" }));
+app.use(express.json({ limit: "5mb" }));
+app.use(express.urlencoded({ extended: true, limit: "5mb" }));
 
-// app.use("/api", router);
+const sequelize = new Sequelize({
+  username: process.env.DB_USERNAME,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASSWORD,
+  port: Number(process.env.DB_PORT),
+  host: process.env.DB_HOST,
+  dialect: "postgres",
+});
 
-app.get("/api", (req: Request, res: Response) => {
+// DB CONNECTION
+(async () => {
+  try {
+    await sequelize.authenticate();
+    console.log("Connection has been established successfully.");
+  } catch (error) {
+    console.error("Unable to connect to the database:", error);
+  }
+})();
+
+sequelize.sync().then(async () => {
+  console.log("RE-synced db.");
+});
+
+app.use("/api", cors(corsOptions), (req: Request, res: Response) => {
   res.json({
-    message: "Made on Earth by human",
+    message: "Its worked!",
   });
 });
 
 app.use("*", (req: Request, res: Response) => {
-  res.json({ message: "Unused Route" });
+  res.json({ message: "Unused Route", madeBy: "Made on Earth by human" }).end();
 });
 
 app.listen(process.env.PORT, () => {
   return console.log(`Server running on http://localhost:${process.env.PORT}`);
 });
+
+export { app, sequelize };
